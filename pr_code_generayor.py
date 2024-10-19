0244a4b8a6fe995c863a9898f1c1fc9ad8ced854
Zakir Aziz <your-email@example.com>
2024-10-19 22:00:00 +0500

    project

--- pr_code_generayor.py
+++ pr_code_generayor.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import qrcode
 
 data = input('Enter the text or URL: ').strip()
@@ -7,3 +8,14 @@ qr.add_data(data)
 image = qr.make_image(fill_color='black', back_color='white')
 image.save(filename)
 print(f'QR code saved as {filename}')
+=======
+import qrcode
+
+data = input('Enter the text or URL: ').strip()
+filename = input('Enter the filename: ').strip()
+qr = qrcode.QRCode(box_size=10, border=4)
+qr.add_data(data)
+image = qr.make_image(fill_color='black', back_color='white')
+image.save(filename)
+print(f'QR code saved as {filename}')
+>>>>>>> 9d801e6 (project)
