d021ce962f697e17edb7dfc50898fd9ad0238502
zakiraziz <118102745+zakiraziz@users.noreply.github.com>
2024-10-21 21:58:54 +0500

    Update cows_and_bulls_game.py

--- cows_and_bulls_game.py
+++ cows_and_bulls_game.py
@@ -2,7 +2,7 @@ import random
 
 def generate_secret():
   digits = list(range(10))
-  random.shuffle(digits)
+  random.shuffle(digits) 
   return ''.join([str(digit) for digit in digits[:4]]) 
  
  
