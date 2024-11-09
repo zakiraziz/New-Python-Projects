20455547c6d8b02101d617e99d2d95288a51df96
Zakir Aziz <your-email@example.com>
2024-11-09 23:15:35 +0500

    project

--- main.py
+++ main.py
@@ -1,66 +1,161 @@
-from settings import *
-from tetris import Tetris, Text
+# Import dependencies
 import sys
-import pathlib
+from PySide6.QtCore import Qt, Slot
+from PySide6.QtGui import QAction, QPainter
+from PySide6.QtWidgets import (QApplication, QHeaderView, QHBoxLayout, QLabel, QLineEdit,  
+                               QMainWindow, QPushButton, QTableWidget, QTableWidgetItem, QVBoxLayout,  
+                               QWidget)
+from PySide6.QtCharts import QChartView, QPieSeries, QChart
 
 
-class App:
+class Widget(QWidget):
     def __init__(self):
-        pg.init()
-        pg.display.set_caption('Tetris')
-        self.screen = pg.display.set_mode(WIN_RES)
-        self.clock = pg.time.Clock()
-        self.set_timer()
-        self.images = self.load_images()
-        self.tetris = Tetris(self)
-        self.text = Text(self)
+        super().__init__()
+        self.items = 0
+        # Dummy data
+        self._data = {"Water": 24, "Rent": 1000, "Coffee": 30, "Grocery": 300, "Phone": 45, "Internet": 70}
 
-    def load_images(self):
-        files = [item for item in pathlib.Path(SPRITE_DIR_PATH).rglob('*.png') if item.is_file()]
-        images = [pg.image.load(file).convert_alpha() for file in files]
-        images = [pg.transform.scale(image, (TILE_SIZE, TILE_SIZE)) for image in images]
-        return images
+        # Table widget
+        self.table = QTableWidget()
+        self.table.setColumnCount(2)
+        self.table.setHorizontalHeaderLabels(["Description", "Price"])
+        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
-    def set_timer(self):
-        self.user_event = pg.USEREVENT + 0
-        self.fast_user_event = pg.USEREVENT + 1
-        self.anim_trigger = False
-        self.fast_anim_trigger = False
-        pg.time.set_timer(self.user_event, ANIM_TIME_INTERVAL)
-        pg.time.set_timer(self.fast_user_event, FAST_ANIM_TIME_INTERVAL)
+        # Chart view
+        self.chart_view = QChartView()
+        self.chart_view.setRenderHint(QPainter.Antialiasing)
 
-    def update(self):
-        self.tetris.update()
-        self.clock.tick(FPS)
+        # Input fields and buttons
+        self.description = QLineEdit()
+        self.price = QLineEdit()
+        self.add = QPushButton("Add")
+        self.clear = QPushButton("Clear")
+        self.quit = QPushButton("Quit")
+        self.plot = QPushButton("Plot")
 
-    def draw(self):
-        self.screen.fill(color=BG_COLOR)
-        self.screen.fill(color=FIELD_COLOR, rect=(0, 0, *FIELD_RES))
-        self.tetris.draw()
-        self.text.draw()
-        pg.display.flip()
+        # Initially disable 'Add' button
+        self.add.setEnabled(False)
 
-    def check_events(self):
-        self.anim_trigger = False
-        self.fast_anim_trigger = False
-        for event in pg.event.get():
-            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
-                pg.quit()
-                sys.exit()
-            elif event.type == pg.KEYDOWN:
-                self.tetris.control(pressed_key=event.key)
-            elif event.type == self.user_event:
-                self.anim_trigger = True
-            elif event.type == self.fast_user_event:
-                self.fast_anim_trigger = True
+        # Right layout
+        self.right = QVBoxLayout()
+        self.right.addWidget(QLabel("Description"))
+        self.right.addWidget(self.description)
+        self.right.addWidget(QLabel("Price"))
+        self.right.addWidget(self.price)
+        self.right.addWidget(self.add)
+        self.right.addWidget(self.plot)
+        self.right.addWidget(self.chart_view)
+        self.right.addWidget(self.clear)
+        self.right.addWidget(self.quit)
 
-    def run(self):
-        while True:
-            self.check_events()
-            self.update()
-            self.draw()
+        # Main layout
+        self.layout = QHBoxLayout()
+        self.layout.addWidget(self.table)
+        self.layout.addLayout(self.right)
+
+        # Set main layout
+        self.setLayout(self.layout)
+
+        # Signals and slots
+        self.add.clicked.connect(self.add_element)
+        self.quit.clicked.connect(self.quit_application)
+        self.plot.clicked.connect(self.plot_data)
+        self.clear.clicked.connect(self.clear_table)
+        self.description.textChanged[str].connect(self.check_disable)
+        self.price.textChanged[str].connect(self.check_disable)
+
+        # Fill table with initial data
+        self.fill_table()
+
+    @Slot()
+    def add_element(self):
+        des = self.description.text()
+        price = self.price.text()
+
+        try:
+            price_item = QTableWidgetItem(f"{float(price):.2f}")
+            price_item.setTextAlignment(Qt.AlignRight)
+
+            self.table.insertRow(self.items)
+            description_item = QTableWidgetItem(des)
+
+            self.table.setItem(self.items, 0, description_item)
+            self.table.setItem(self.items, 1, price_item)
+
+            self.description.setText("")
+            self.price.setText("")
+
+            self.items += 1
+        except ValueError:
+            print("Invalid input for price:", price)
+
+    @Slot()
+    def check_disable(self):
+        self.add.setEnabled(bool(self.description.text() and self.price.text()))
+
+    @Slot()
+    def plot_data(self):
+        series = QPieSeries()
+        for i in range(self.table.rowCount()):
+            text = self.table.item(i, 0).text()
+            number = float(self.table.item(i, 1).text())
+            series.append(text, number)
+
+        chart = QChart()
+        chart.addSeries(series)
+        chart.legend().setAlignment(Qt.AlignLeft)
+        self.chart_view.setChart(chart)
+
+    @Slot()
+    def quit_application(self):
+        QApplication.quit()
+
+    def fill_table(self):
+        for desc, price in self._data.items():
+            description_item = QTableWidgetItem(desc)
+            price_item = QTableWidgetItem(f"{price:.2f}")
+            price_item.setTextAlignment(Qt.AlignRight)
+            self.table.insertRow(self.items)
+            self.table.setItem(self.items, 0, description_item)
+            self.table.setItem(self.items, 1, price_item)
+            self.items += 1
+
+    @Slot()
+    def clear_table(self):
+        self.table.setRowCount(0)
+        self.items = 0
 
 
-if __name__ == '__main__':
-    app = App()
-    app.run()
+class MainWindow(QMainWindow):
+    def __init__(self, widget):
+        super().__init__()
+        self.setWindowTitle("Expense Tracker")
+
+        # Menu
+        self.menu = self.menuBar()
+        self.file_menu = self.menu.addMenu("File")
+
+        # Exit action
+        exit_action = QAction("Exit", self)
+        exit_action.setShortcut("Ctrl+Q")
+        exit_action.triggered.connect(self.exit_app)
+
+        self.file_menu.addAction(exit_action)
+        self.setCentralWidget(widget)
+
+    @Slot()
+    def exit_app(self, checked):
+        QApplication.quit()
+
+
+if __name__ == "__main__":
+    # Initialize Qt application
+    app = QApplication(sys.argv)
+    # Create main widget and window
+    widget = Widget()
+    window = MainWindow(widget)
+    window.resize(800, 600)
+    window.show()
+
+    # Run application
+    sys.exit(app.exec())
